3dacb6e06458ccb598b087b5a33ec53f145c3542
Chathurika Senani <zenaronline@gmail.com>
2022-12-03 15:14:58 +0530

    feat: add chart service to read result

--- src/main/java/edu/iit/workload/api/ChartController.java
+++ src/main/java/edu/iit/workload/api/ChartController.java
@@ -1,63 +1,30 @@
 package edu.iit.workload.api;
 
 import com.fasterxml.jackson.core.JsonProcessingException;
-import com.fasterxml.jackson.databind.JsonNode;
-import com.fasterxml.jackson.databind.ObjectMapper;
+import edu.iit.workload.service.ChartService;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
+import java.util.List;
+import java.util.Map;
+
 @Slf4j
 @RestController
 @RequestMapping(value = "/api/v1")
 public class ChartController {
 
-    @GetMapping("/{type}")
-    public JsonNode chart(@PathVariable String type) throws JsonProcessingException {
+	private final ChartService chartService;
 
-        ObjectMapper mapper = new ObjectMapper();
-        JsonNode node = mapper.readValue("[\n" +
-                "   {\n" +
-                "      \"name\":\"MC\",\n" +
-                "      \"IQR\":4000,\n" +
-                "      \"LR\":2400,\n" +
-                "      \"MAD\":1400,\n" +
-                "      \"LRR\":400,\n" +
-                "      \"THR\":300,\n" +
-                "      \"GA\":100\n" +
-                "   },\n" +
-                "   {\n" +
-                "      \"name\":\"MMT\",\n" +
-                "      \"IQR\":3000,\n" +
-                "      \"LR\":1398,\n" +
-                "      \"MAD\":1698,\n" +
-                "      \"LRR\":5398,\n" +
-                "      \"THR\":6398,\n" +
-                "      \"GA\":210\n" +
-                "   },\n" +
-                "   {\n" +
-                "      \"name\":\"MU\",\n" +
-                "      \"IQR\":2000,\n" +
-                "      \"LR\":9800,\n" +
-                "      \"MAD\":7800,\n" +
-                "      \"LRR\":9800,\n" +
-                "      \"THR\":3000,\n" +
-                "      \"GA\":900\n" +
-                "   },\n" +
-                "   {\n" +
-                "      \"name\":\"RS\",\n" +
-                "      \"IQR\":2780,\n" +
-                "      \"LR\":3908,\n" +
-                "      \"MAD\":708,\n" +
-                "      \"LRR\":1908,\n" +
-                "      \"THR\":908,\n" +
-                "      \"GA\":70\n" +
-                "   }\n" +
-                "]", JsonNode.class);
-        return node;
+	public ChartController(ChartService chartService) {
+		this.chartService = chartService;
+	}
 
-    }
+	@GetMapping("/{type}")
+	public List<Map<String, Object>> chart(@PathVariable String type) throws JsonProcessingException {
+		return chartService.chart(type);
+	}
 
 }
---
+++ src/main/java/edu/iit/workload/service/ChartService.java
@@ -0,0 +1,43 @@
+package edu.iit.workload.service;
+
+import edu.iit.workload.domain.ResultAnalysis;
+import edu.iit.workload.repository.ResultDataRepository;
+import org.springframework.stereotype.Service;
+
+import java.util.List;
+import java.util.Map;
+import java.util.stream.Collectors;
+
+@Service
+public class ChartService {
+
+	private final ResultDataRepository resultDataRepository;
+
+	public ChartService(ResultDataRepository resultDataRepository) {
+		this.resultDataRepository = resultDataRepository;
+	}
+
+	public List<Map<String, Object>> chart(String type) {
+
+		List<ResultAnalysis> entities = resultDataRepository.findAll();
+
+		List<Map<String, Object>> result = entities.stream().map(ResultAnalysis::getVmSelectionPolicy).distinct()
+				.map(m -> {
+					Map<String, Object> map = entities.stream().filter(r -> r.getVmSelectionPolicy().equals(m)).collect(
+							Collectors.toMap(a -> a.getVmAllocationPolicy().toUpperCase(), a -> findValue(type, a)));
+					map.put("name", m);
+					return map;
+				}).toList();
+
+		return result;
+
+	}
+
+	private double findValue(String type, ResultAnalysis resultAnalysis) {
+		if ("energy".equals(type)) {
+			return resultAnalysis.getEnergy();
+		}
+		return 0;
+	}
+
+}
